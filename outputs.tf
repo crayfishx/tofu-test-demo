232976e98481270f801f185523b21cbe7e6a0b56
Craig Dunn <craig@craigdunn.org>
2024-01-30 15:46:44 +0100

    added http

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "endpoint_url" {
   description = "S3 Endpoint URL"
-  value = aws_s3_bucket_website_configuration.website.website_endpoint
+  value = "http://${aws_s3_bucket_website_configuration.website.website_endpoint}"
 }
